40f9054a919a8c5f8b0115e6b122f15a9923f9eb
yanyunchangfeng <yanyunchangfeng@gmail.com>
2023-06-24 19:51:06 +0800

    feat:add travis CICD

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ document.body.innerHTML = `<div id="root"></div>`;
 const App: FC = () => {
   return <></>;
 };
-const root = createRoot(document.getElementById("root")!);
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
 root.render(<App />);
 
 export { App };
