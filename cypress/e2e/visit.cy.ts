c8874b46effb0d490de4446356521d2cb6f7e52f
yanyunchangfeng <yanyunchangfeng@gmail.com>
2023-07-04 12:04:13 +0800

    feat: add prettier lint-staged for code format

--- cypress/e2e/visit.cy.ts
+++ cypress/e2e/visit.cy.ts
@@ -3,19 +3,19 @@ interface IVisit {
   icon: string;
 }
 
-describe("test visit / ", () => {
+describe('test visit / ', () => {
   beforeEach(() => {
-    cy.fixture("visit").as("visit");
-    cy.visit("/");
+    cy.fixture('visit').as('visit');
+    cy.visit('/');
   });
-  it("get div id correct", () => {
-    cy.get<IVisit>("@visit").then((visit) => {
-      cy.get("div").should("have.id", visit.id);
+  it('get div id correct', () => {
+    cy.get<IVisit>('@visit').then((visit) => {
+      cy.get('div').should('have.id', visit.id);
     });
   });
-  it("get link icon correct", () => {
-    cy.get<IVisit>("@visit").then((visit) => {
-      cy.get("link[rel='icon']").should("have.attr", "href", visit.icon);
+  it('get link icon correct', () => {
+    cy.get<IVisit>('@visit').then((visit) => {
+      cy.get("link[rel='icon']").should('have.attr', 'href', visit.icon);
     });
   });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import { FC } from "react";
-import { createRoot } from "react-dom/client";
-import React from "react";
-import "./index.scss";
+import { FC } from 'react';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import './index.scss';
 document.body.innerHTML = `<div id="root"></div>`;
 const App: FC = () => {
   return <></>;
 };
-const container = document.getElementById("root") as HTMLElement;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
 
