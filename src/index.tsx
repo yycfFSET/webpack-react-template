13c8b16d095959967085733a05374c2ddc69c823
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-07-28 08:42:42 +0800

    add eslint from & code analysis ci

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import App from 'src/App';
 
 const container = document.getElementById('root') as HTMLElement;
